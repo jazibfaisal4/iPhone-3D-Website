7083fbe6992db6b083298bfeaf80cbf42e3ec803
jazibfaisal4 <jazibfaisal4@gmail.com>
2026-03-27 19:37:22 +0500

    Fix again overflow issue

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -19,7 +19,7 @@ const Highlights = () => {
     return () => mm.revert();
   }, [])
   return (
-    <section id="highlights" className="w-screen overflow-hidden h-full common-padding bg-zinc">
+    <section id="highlights" className="w-full max-w-[100vw] overflow-x-hidden overflow-hidden h-full common-padding bg-zinc">
       <div className="screen-max-width">
         <div className="mb-12 w-full md:flex items-end justify-between">
           <h1 id="title" className="section-heading">Get the highlights.</h1>
